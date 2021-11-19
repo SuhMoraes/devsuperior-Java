decdcea0025c7312c570cdc509bb5f0b019bc223
SuhMoraes <suh.alvesmoraes@gmail.com>
2021-11-19 07:07:06 -0300

    Java-OOP

---
+++ java-oop/src/estruturasequencial/DataOutput.java
@@ -0,0 +1,15 @@
+package estruturasequencial;
+
+public class DataOutput {
+    public static void main(String[] args){
+
+        int y = 32;
+        double x = 10.45102;
+
+        System.out.println(y);
+        System.out.println("Bom dia!");
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);
+
+    }
+}
